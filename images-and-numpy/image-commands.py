fd6ceef963e40bdc63fb4307f04e262134dd0727
Adrian Grillo <grillo.adrian13@gmail.com>
2021-11-05 17:10:56 -0400

    basic image commands

---
+++ images-and-numpy/image-commands.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+pic = Image.open('DATA/00-puppy.jpg')
+pic_arr = np.asarray(pic)
+
+plt.imshow(pic_arr)
+
+# This results in an image being displayed
+
+pic_arr.shape
+
+# (1300, 1950, 3) 3 iimages to makeup one, each image contains only red, blue, in green varying in different amounts.
+# Use array slicing to manipulate a certain color channel and zero our certain indexes to remove color from certain spots in the image
