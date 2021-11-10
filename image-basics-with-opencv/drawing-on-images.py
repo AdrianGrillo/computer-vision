55e850afea44f9ef532f4964d0e6a748e17e07d7
Adrian Grillo <grillo.adrian13@gmail.com>
2021-11-09 20:29:31 -0500

    drawing polygons on an image

--- image-basics-with-opencv/drawing-on-images.py
+++ image-basics-with-opencv/drawing-on-images.py
@@ -13,3 +13,12 @@ cv2.circle(blank_img, (100,100), 50, color=(0,255,255), thickness=-1)
 
 # Draw a line from the top left of the image to the bottom right
 cv2.line(blank_img, pt1=(0,512), pt2=(512,0), color=(0,0,255), thickness=10)
+
+# Define tuples that each correspond to a point of a polygon
+vertices = np.array([ [100,300], [200,200], [400,300], [200,400] ], dtype=np.int32)
+
+# Reshape to be in the format that cv2 expects
+points = vertices.reshape(-1,1,2)
+
+# Draw the polygon onto blank_img. Note: point values must be passed in an array
+cv2.polylines(blank_img, [points], isClosed=True, color=(255,0,0), thickness=5)
