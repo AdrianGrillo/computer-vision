8de3e8303a352837c3d19206c1506b6203e0846f
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2021-11-11 23:29:27 -0500

    Update image-blurring.py

--- image-processing/image-blurring.py
+++ image-processing/image-blurring.py
@@ -7,7 +7,7 @@ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Import an image and place text across it to make blurring more obvious
 font = cv2.FONT_HERSHEY_COMPLEX
-cv2.putText(img, text='tight pussy', org=(10, 600), fontFace=font, fontScale=10, color=(255, 0, 0), thickness=4)
+cv2.putText(img, text='bricks', org=(10, 600), fontFace=font, fontScale=10, color=(255, 0, 0), thickness=4)
 
 # Define kernel
 kernel = np.ones(shape=(5,5), dtype=np.float32) / 25
