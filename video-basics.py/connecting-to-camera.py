e7802d108ebd0e87a323ed1564564ea8f6512424
AdrianGrillo <grillo.adrian13@gmail.com>
2021-11-13 14:56:33 -0500

    script to record webcam capture data and write to file

---
+++ video-basics.py/connecting-to-camera.py
@@ -0,0 +1,28 @@
+import cv2
+
+# Start reading video data from default camera device
+cap = cv2.VideoCapture(0)
+
+# Get the width and height of the video data being returned from capture and convert it from a floating point to integer
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# Create writer object. String passed to second param is OS dependent, since I'm on windows it DIVX. 
+writer = cv2.VideoWriter('mysupervideo.mp4'. cv2.VideoWriter_fourcc(*'DIVX'), 30, (width, height))
+
+# Write and display video capture data
+while True: 
+    ret, frame = cap.read()
+
+    # Any sort of operations to be performed to the capture data (like drawing on it or object detection) would go here before it's written or displayed 
+    writer.write(frame)
+    cv2.imshow(frame, frame)
+    
+    # If the user presses q, then break out of the while loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release capture and write object then close the window that's displaying the video capture data
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
